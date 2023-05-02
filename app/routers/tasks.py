411b91ca5c62e20fd1cc567b806d05fdda7208dd
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-05-03 01:31:05 +0300

    Make translate endpoint protected

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -18,7 +18,7 @@ async def speech_to_text(
     return STTTranscript(text=response)
 
 @router.post("/translate", response_model=TranslationResponse)
-async def translate_(translation_request: TranslationRequest):
+async def translate_(translation_request: TranslationRequest, current_user = Depends(get_current_user)):
 
     response = translate(translation_request.text, translation_request.source_language, translation_request.target_language)
     return TranslationResponse(text=response)
