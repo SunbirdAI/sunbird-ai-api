69934c802febf654e67784272eeca07ae9702b90
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-05-19 13:41:15 +0300

    Update homepage docs

--- app/docs.py
+++ app/docs.py
@@ -7,22 +7,20 @@ Welcome to the Sunbird AI API documentation. The Sunbird AI API provides you acc
 You can create an account and test the endpoints directly on this page.
 
 ## Getting started
+You can checkout the [usage guide](https://github.com/SunbirdAI/sunbird-ai-api/blob/main/tutorial.md) for a full tutorial.
+
 ### Signing up
 If you don't already have an account, use the `/auth/register` endpoint to create one. (You can scroll down this page to try it out)
 
 ### Logging in and getting an access token.
 Authentication is done via a Bearer token. Use the `/auth/token` endpoint to get your access token. This token lasts for 7 days.
 
+Use the `Authorize` button below to login and access the protected endpoints.
 
 ### AI Tasks
 - Use the `/tasks/stt` endpoint for speech to text inference for one audio file.
 - Use the `/tasks/translate` endpoint for translation of one text input.
 - Use the `/tasks/translate-batch` endpoint for translation of multiple text inputs.
-
-You can use the interactive documentation below to test out these endpoints and get a feel for their structure. 
-You can use the `Authorize` button below to login and access the protected endpoints.
-
-Also checkout this repository (TODO) for code samples in Python and Javascript.
 """
 
 tags_metadata = [
