b0fc5790644ecdf1f8efc6441eb79db5ebff376e
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-06-29 16:01:53 +0300

    Fix CORs error

--- app/api.py
+++ app/api.py
@@ -4,6 +4,7 @@ from app.routers.tasks import router as tasks_router
 from app.routers.auth import router as auth_router
 from app.middleware.monitoring_middleware import log_request
 from app.docs import description, tags_metadata
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -14,6 +15,16 @@ app = FastAPI(
 
 logging_middleware = partial(log_request)
 app.middleware("http")(logging_middleware)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(tasks_router, prefix="/tasks", tags=["AI Tasks"])
 app.include_router(auth_router, prefix="/auth", tags=["Authentication Endpoints"])
