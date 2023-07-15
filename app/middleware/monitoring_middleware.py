41896313f59d943a43da925a47189694608effc3
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-07-15 18:04:46 +0300

    Temporary fix for queuepool limit exceeded

--- app/middleware/monitoring_middleware.py
+++ app/middleware/monitoring_middleware.py
@@ -5,6 +5,8 @@ from app.routers.auth import get_current_user
 from app.deps import get_db
 from app.schemas.monitoring import EndpointLog
 from app.crud.monitoring import create_endpoint_log
+from jose import jwt
+from app.utils.auth_utils import SECRET_KEY, ALGORITHM
 
 
 async def log_request(request: Request, call_next):
@@ -12,15 +14,20 @@ async def log_request(request: Request, call_next):
         try:
             token = request.headers['Authorization'].replace('Bearer ', '')
             db_session = next(get_db())
-            user = get_current_user(token, db_session)
+
+            # TODO: Find another way of getting the current user. This is inefficient as it makes 2 similar database calls which causes problems with the DB pool size
+            # user = get_current_user(token, db_session)
+            # TODO: This is a hacky workaround for the hackathon to prevent multiple DB calls.
+            username = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]).get("sub")
+            organization = "PLACEHOLDER_TO_FIX"
             start = time.time()
             response = await call_next(request)
             end = time.time()
             # print(current_user.username)
             endpoint_log = EndpointLog(
-                username=user.username,
+                username=username,
                 endpoint=request.url.path,
-                organization=user.organization,
+                organization=organization,
                 time_taken=(end - start)
             )
             create_endpoint_log(db_session, endpoint_log)
