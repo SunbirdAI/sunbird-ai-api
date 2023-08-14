bad6ad8748f5ec4fe600f2841184a445693a4259
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-08-14 12:20:36 +0300

    Setup jinja templating with tailwindcss

--- app/api.py
+++ app/api.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, Request
 from functools import partial
 from app.routers.tasks import router as tasks_router
 from app.routers.auth import router as auth_router
+from app.routers.frontend import router as frontend_router
 from app.middleware.monitoring_middleware import log_request
 from app.docs import description, tags_metadata
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI(
@@ -12,6 +14,8 @@ app = FastAPI(
     description=description,
     openapi_tags=tags_metadata
 )
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 logging_middleware = partial(log_request)
 app.middleware("http")(logging_middleware)
@@ -26,5 +30,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
 app.include_router(tasks_router, prefix="/tasks", tags=["AI Tasks"])
 app.include_router(auth_router, prefix="/auth", tags=["Authentication Endpoints"])
+app.include_router(frontend_router, prefix="", tags=["Frontend Routes"])
---
+++ app/routers/frontend.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
+
+@router.get("/")
+async def home(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("base.html", context)
