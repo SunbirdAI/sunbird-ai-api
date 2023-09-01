0eb3dfe694bb3f6dfce8b9132be265d1cd8f08a8
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-09-01 14:35:54 +0300

    Add logout functionality

--- app/utils/monitoring_utils.py
+++ app/utils/monitoring_utils.py
@@ -7,6 +7,7 @@ from app.schemas.monitoring import EndpointLog
 
 
 def aggregate_usage_for_user(db: Session, username: str):
+    # TODO: Make this load only data for the current month.
     logs = get_logs_by_username(db, username)
     logs = [EndpointLog.from_orm(endpoint_log) for endpoint_log in logs]
     aggregates = defaultdict(int)
